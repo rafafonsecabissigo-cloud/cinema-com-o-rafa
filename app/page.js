a89ed769ad9cd6420b65415de127774c49be0d16
Rafael Bissigo <cinemacomorafa@gmail.com>
2026-04-17 16:27:22 -0300

    Fix app/page imports for Vercel build

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import Navbar from "../../components/Navbar";
-import Footer from "../../components/Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 
 export const metadata = {
   title: "Sobre | Cinema com o Rafa",
